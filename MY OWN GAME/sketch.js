ac2f76bcc2adfab51600bcc8cdc01396f314274d
Aashvi27 <70275920+Aashvi27@users.noreply.github.com>
2021-01-07 12:11:40 +0530

    Add files via upload

---
+++ MY OWN GAME/sketch.js
@@ -0,0 +1,28 @@
+
+var plyer,O1,O2,O3,O4,O6,S1,septagon.png,squar.pg,octagon.png,rectangle.png,hexagon.png,circle.png
+
+function preload()
+{
+boy.jpg= loadImage("boy.jpg");	
+fruit1 = loadImage("fruit1.png");
+fruit1 = loadImage("fruit1.png");
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	
+  
+}
+
+
+function draw() {
+ 
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
